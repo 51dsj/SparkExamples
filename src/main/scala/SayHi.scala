a5d60eeae77af27f8f7c1e432e40b24d5c0d1e10
zhangrunqin <zhangrunqin713@pingan.com.cn>
2016-12-02 17:45:46 +0800

    SayHi

---
+++ src/main/scala/SayHi.scala
@@ -0,0 +1,9 @@
+import org.apache.spark.SPARK_VERSION
+/**
+  * Created by zhangrunqin on 16-12-2.
+  */
+object SayHi {
+  def main(args: Array[String]) {
+    println("hi,spark version = " + SPARK_VERSION)
+  }
+}
